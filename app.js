ec0c94c5f1e352e6eba1245b8bebbd4f25857fef
Darshan Jain <darshanj@heaptrace.com>
2022-04-01 13:40:17 +0530

    Added substract function

--- app.js
+++ app.js
@@ -9,17 +9,14 @@ function setNumber(number){
 }
 
 function add(){
-  debugger
   var input=getNumber();
   var output=input.number1+input.number2;
   setNumber(output)
 }
-<<<<<<< HEAD
+
 
 function sub(){
   var input=getNumber();
   var output=input.number1-input.number2;
   setNumber(output)
 }
-=======
->>>>>>> 14d747fa2997fc3769510d4816f3c448c0fc911f
