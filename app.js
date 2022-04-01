2c1546adc23e8ae67fdb1222f61fbcfc6f76b74a
Darshan Jain <darshanj@heaptrace.com>
2022-04-01 13:28:43 +0530

    Added substraction function

---
+++ app.js
@@ -0,0 +1,22 @@
+function getNumber(){
+  var number1=parseInt(document.getElementById("number1").value)
+  var number2=parseInt(document.getElementById("number2").value)
+  return {"number1":number1,"number2":number2}
+}
+
+function setNumber(number){
+  document.getElementById("answer").innerHTML=number
+}
+
+function add(){
+  debugger
+  var input=getNumber();
+  var output=input.number1+input.number2;
+  setNumber(output)
+}
+
+function sub(){
+  var input=getNumber();
+  var output=input.number1-input.number2;
+  setNumber(output)
+}
